48d2831bbe29cb8d3d8048bf7f412d2c06d1f98f
Van Tien Tu <vantientu1703@gmail.com>
2016-08-12 14:34:26 +0700

    create login view controller

---
+++ GymManager/GymManager/ViewControllers/LoginViewController/LoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.m
+//  GymManager
+//
+//  Created by Văn Tiến Tú on 8/12/16.
+//  Copyright © 2016 vantientu. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldUserName;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldPassword;
+@property (weak, nonatomic) IBOutlet UILabel *labelNotes;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)loginPress:(id)sender {
+    //TODO
+}
+
+- (IBAction)registerPress:(id)sender {
+    //TODO
+}
+
+- (IBAction)forgetPasswordPress:(id)sender {
+    //TODO
+}
+
+@end
